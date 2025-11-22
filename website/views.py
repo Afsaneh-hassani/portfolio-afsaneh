842f75b9ce9b548b27e56c9446328dfc709387a0
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-11-22 20:10:24 +0330

    starting django project
    starting django project for portfolio

---
+++ website/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+def index_view(request):
+    return render(request,'website/index.html')
+
+
+# Create your views here.
